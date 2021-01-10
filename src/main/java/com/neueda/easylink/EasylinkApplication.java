eb05a645d1b48eb0650fb1793e132ddcb60fd2c2
James O'Neill <james@LJONEILL.ie.murex.com>
2021-01-10 15:14:05 +0000

    Repository Setup

---
+++ src/main/java/com/neueda/easylink/EasylinkApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.easylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasylinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasylinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neueda/easylink/EasylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.easylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasylinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
